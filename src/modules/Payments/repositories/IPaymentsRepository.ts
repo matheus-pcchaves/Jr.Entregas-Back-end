b6618bcd2f5bbe5f4f571b08395738cd3e099535
Matheus <vicde2@hotmail.com>
2022-07-28 10:08:51 -0300

    Cadastro e relacionamento de pagamentos

--- src/modules/Payments/repositories/IPaymentsRepository.ts
+++ src/modules/Payments/repositories/IPaymentsRepository.ts
@@ -1,7 +1,7 @@
 import { IPaymentsDTO } from "../dtos/IPaymentsDTO";
 import { Payment } from "../entities/Payment";
 
-class IPaymentsRepository {
+interface IPaymentsRepository {
     create(data: IPaymentsDTO): Promise<void>
     findByName(name: string): Promise<Payment>
     list(): Promise<Payment[]>
