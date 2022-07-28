d687b974e09b209872029865dafde81a88ef0b54
Matheus <vicde2@hotmail.com>
2022-07-28 13:57:58 -0300

    Criação de entregas e testes

--- src/modules/RequestsDeliveries/useCases/Deliveries/CreateDeliveries/CreateDeliveriesUseCase.spec.ts
+++ src/modules/RequestsDeliveries/useCases/Deliveries/CreateDeliveries/CreateDeliveriesUseCase.spec.ts
@@ -1,13 +1,69 @@
+import { AppError } from "@errors/AppError"
+import { DeliveriesRepositoryInMemory } from "@modules/RequestsDeliveries/repositories/In-memory/DeliveriesRepositoryInMemory"
+import { RequestsRepositoryInMemory } from "@modules/RequestsDeliveries/repositories/In-memory/RequestsRepositoryInMemory"
+import { DayjsDateProvider } from "@shared/container/providers/DateProvider/implementations/DayjsDateProvider"
+import dayjs from "dayjs"
 import { CreateDeliveriesUseCase } from "./CreateDeliveriesUseCase"
 
 let createDeliveriesUseCase: CreateDeliveriesUseCase
+let deliveriesRepositoryInMemory: DeliveriesRepositoryInMemory
+let dayjsDateProvider: DayjsDateProvider
+let requestsRepository: RequestsRepositoryInMemory
 
 describe("Create a new delivery", () => {
+    const dayAdd24Hours = dayjs().add(1, "day").toDate()
+
     beforeEach(() => {
-        createDeliveriesUseCase = new CreateDeliveriesUseCase()
+        deliveriesRepositoryInMemory = new DeliveriesRepositoryInMemory()
+        dayjsDateProvider = new DayjsDateProvider()
+        requestsRepository = new RequestsRepositoryInMemory()
+        createDeliveriesUseCase = new CreateDeliveriesUseCase(deliveriesRepositoryInMemory, dayjsDateProvider, requestsRepository)
     })
 
     it("Should be able to create a new delivery", async () => {
-        await createDeliveriesUseCase.execute()
+        const request = await requestsRepository.create({
+            item_description: "Test",
+            address: "Test address",
+            city_id: "12345",
+            payment_id: "54321"
+        })
+
+        const delivery = await deliveriesRepositoryInMemory.create({
+            request_id: request.id,
+            deliveryman_id: "deliveryman_id_test",
+            expected_finish_date: dayAdd24Hours
+        })
+
+        expect(delivery).toHaveProperty("id")
+    })
+
+    it("Should not be able to create a new delivery if delivery is already in progress", async () => {
+        await deliveriesRepositoryInMemory.create({
+            request_id: "4567",
+            deliveryman_id: "1234",
+            expected_finish_date: dayAdd24Hours
+        })
+
+        await expect(createDeliveriesUseCase.execute({
+            request_id: "4567",
+            deliveryman_id: "4321",
+            expected_finish_date: dayAdd24Hours
+        })
+        ).rejects.toBeInstanceOf(AppError)
+    })
+
+    it("Should not be able to create a new delivery if deliveryman is not available", async () => {
+        await deliveriesRepositoryInMemory.create({
+            request_id: "4567",
+            deliveryman_id: "1234",
+            expected_finish_date: dayAdd24Hours
+        })
+
+        await expect(createDeliveriesUseCase.execute({
+            request_id: "7654",
+            deliveryman_id: "1234",
+            expected_finish_date: dayAdd24Hours
+        })
+        ).rejects.toBeInstanceOf(AppError)
     })
 })
--- src/modules/RequestsDeliveries/useCases/Deliveries/CreateDeliveries/CreateDeliveriesUseCase.ts
+++ src/modules/RequestsDeliveries/useCases/Deliveries/CreateDeliveries/CreateDeliveriesUseCase.ts
@@ -1,5 +1,64 @@
+import { AppError } from "@errors/AppError";
+import { Delivery } from "@modules/RequestsDeliveries/entities/Delivery";
+import { IDeliveriesRepository } from "@modules/RequestsDeliveries/repositories/IDeliveriesRepository";
+import { IRequestsRepository } from "@modules/RequestsDeliveries/repositories/IRequestsRepository";
+import { IDateProvider } from "@shared/container/providers/DateProvider/IDateProvider";
+import { inject, injectable } from "tsyringe";
+
+interface IRequest {
+    request_id: string,
+    deliveryman_id: string,
+    expected_finish_date: Date          
+}
+
+@injectable()
 class CreateDeliveriesUseCase {
-    async execute(){}
+
+    constructor(
+        @inject("DeliveriesRepository")
+        private deliveriesRepository: IDeliveriesRepository,
+        @inject("DayjsDateProvider")
+        private dateProvider: IDateProvider,
+        @inject("RequestsRepository")
+        private requestsRepository: IRequestsRepository
+    ){}
+
+    async execute({ request_id, deliveryman_id, expected_finish_date }: IRequest): Promise<Delivery>{
+        const minimumHours = 24
+
+        const requestAlreadyInProgress = await this.deliveriesRepository.findRequestInProgress(request_id)
+
+        if(!requestAlreadyInProgress){
+            throw new AppError('Request already in progress')
+        }
+
+        const deliverymanUnavailable = await this.deliveriesRepository.findDeliverymanAvailable(deliveryman_id)
+
+        if(!deliverymanUnavailable){
+            throw new AppError('Deliveryman is unavailable')
+        }
+
+        const dateNow = this.dateProvider.dateNow()
+
+        const compare = this.dateProvider.compareInHours(
+            dateNow,
+            expected_finish_date,
+        )
+
+        if(compare < minimumHours){
+            throw new AppError('Invalid return time')
+        }
+
+        const newDelivery = await this.deliveriesRepository.create({
+            request_id,
+            deliveryman_id,
+            expected_finish_date
+        })
+
+        await this.requestsRepository.updateStatus(request_id, false)
+
+        return newDelivery
+    }
 }
 
 export { CreateDeliveriesUseCase }
